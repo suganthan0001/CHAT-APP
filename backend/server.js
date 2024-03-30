61a54342370d2fe803b9f2350a230e2e4f01b0d5
Suganthan <suganthan0001@gmail.com>
2024-03-30 22:28:26 +0530

    ready to deploy

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from 'express';
 import dotenv from 'dotenv';
 import messageRoutes from "./routes/message.routes.js";
@@ -7,6 +8,8 @@ import connectToDB from "./db/connectToMongoDB.js";
 import userRoutes from "./routes/user.routes.js";
 import cors from "cors";
 import { app,server } from './socket/socket.js';
+
+const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -19,7 +22,11 @@ app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 app.use("/api/users", userRoutes)
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
 
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend","dist","index.html"))
+})
 app.get("/", (req,res)  => {
     res.send("SERVER is READY");
 })
