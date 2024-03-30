72146494be9ee137562381da8403578b8084eb24
Suganthan <suganthan0001@gmail.com>
2024-03-30 23:05:55 +0530

    final deployment for laptops

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,17 +7,32 @@ import {Toaster} from "react-hot-toast"
 import { useAuthContext } from "./context/AuthContext";
 
 function App() {
+  const { authUser } = useAuthContext();
 
-  const {authUser} = useAuthContext();
-  
+  const isSmallScreen = window.innerWidth < 700;
+
   return (
     <>
       <div className="h-screen flex items-center justify-center">
-        <Routes>
-          <Route path="/" element = {authUser?<Home />:<Navigate to="/login"/>}/>
-          <Route path="/login" element = {authUser? <Navigate to ="/"/>:<Login />}/>
-          <Route path="/signup" element = {authUser ? <Navigate to ="/"/>:<Signup />}/>
-        </Routes>
+        {isSmallScreen ? (<>
+          <p className="text-white text-3xl">COMING SOON...<br />(Please view on larger screens) </p>
+        </>
+        ) : (
+          <Routes>
+            <Route
+              path="/"
+              element={authUser ? <Home /> : <Navigate to="/login" />}
+            />
+            <Route
+              path="/login"
+              element={authUser ? <Navigate to="/" /> : <Login />}
+            />
+            <Route
+              path="/signup"
+              element={authUser ? <Navigate to="/" /> : <Signup />}
+            />
+          </Routes>
+        )}
         <Toaster />
       </div>
     </>
